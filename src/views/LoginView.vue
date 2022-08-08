e6df6d322dde786608329ba16269c7deae251209
jinx <a239060538@icloud.com>
2022-08-09 02:13:04 +0800

    表单

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -5,6 +5,33 @@
         <li :class="{current:item.current}" v-for="item in menuTab" :key="item.id" @click="toggleMneu(item)">{{ item.txt }}</li>
        
       </ul>
+      <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm"  class="demo-ruleForm">
+  
+        <el-form-item  prop="email" class="item-from">
+          <label for="">邮箱</label>
+          <el-input type="text" v-model="ruleForm.email" autocomplete="off"></el-input>
+        </el-form-item>
+
+        <el-form-item  prop="Pass" class="item-from">
+          <label for="">密码</label>
+          <!-- <el-input type="password" v-model="ruleForm.Pass" autocomplete="off"></el-input> -->
+          <el-input placeholder="请输入密码" v-model="ruleForm.Pass" show-password maxlength="10" minlengh="6"></el-input>
+
+        </el-form-item>
+
+        <el-form-item  prop="code" class="item-from">
+          
+        </el-form-item>
+        <label for="">验证码</label>
+          <el-row :gutter="24">
+          <el-col :span="15"><el-input v-model.number="ruleForm.code" maxlength="6" minlengh="6"></el-input></el-col>
+          <el-col :span="9"><el-button type="success" class="block" > 获取验证码</el-button></el-col>
+        </el-row>
+          
+        <el-form-item>
+          <el-button type="success" @click="submitForm('ruleForm')" class="sub-button">提交</el-button>
+        </el-form-item>
+    </el-form>
     </div>
   </div>
 </template>
@@ -12,7 +39,60 @@
 export default{
   name:'login',
   data(){
+      var code = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('验证码不能为空'));
+        }
+        setTimeout(() => {
+          if (!Number.isInteger(value)) {
+            callback(new Error('请输入数字值'));
+          } else {
+            if (value < 18) {
+              callback(new Error('必须年满18岁'));
+            } else {
+              callback();
+            }
+          }
+        }, 1000);
+      };
+      var email = (rule, value, callback) => {
+        let  str = "^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.(com|cn|net)$";
+        if (value === '') {
+          callback(new Error('请输入邮箱'));
+        } else if(str.value){
+          callback(new Error('邮箱格式有误'));
+          if (this.ruleForm.checkPass !== '') {
+            this.$refs.ruleForm.validateField('checkPass');
+          }
+          callback();
+        }
+      };
+      var Pass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } else if (value !== this.ruleForm.pass) {
+          callback(new Error('两次输入密码不一致!'));
+        } else {
+          callback();
+        }
+      };
     return{
+      ruleForm: {
+          email: '',
+          Pass: '',
+          code: ''
+        },
+        rules: {
+          email: [
+            { validator: email, trigger: 'blur' }
+          ],
+          Pass: [
+            { validator: Pass, trigger: 'blur' }
+          ],
+          code: [
+            { validator: code, trigger: 'blur' }
+          ]
+        },
       menuTab:[
         {txt:'登录',current:false},
         {txt:'注册',current:false}
@@ -29,7 +109,18 @@ export default{
       });
       data.current=true
     }
-  }
+  },
+  submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      }
+    
 }
 </script>
 <style lang="scss" scoped>
@@ -37,6 +128,11 @@ export default{
 .about {
   height: 100vh;
   background-color: #344a5f;
+  display: flex;
+  
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 .login-warp{
   width: 330xp;
@@ -57,6 +153,24 @@ export default{
   .current {
     background-color: rgba($color: #000000, $alpha: 0.5);
   }
+}
+.demo-ruleForm {
+  margin-top: 20px;
+  label {
+    display: block;
+    margin-bottom: 3px;
+    font-size: 14px;
+    color: #fff;
+  }
+}
+.item-from{
+  margin-bottom: 13px;
+  color: #fff;
+}
+.sub-button{
+  display: block;
+  width: 100%;
+  margin-top: 19px;
 }
 
 </style>
